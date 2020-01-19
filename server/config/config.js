5717c35c5cb67643481f624e8226d393d853b745
Obeth Ortiz <obetho@gmail.com>
2020-01-19 15:24:53 -0600

    Variable entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://sa:8m3RIVf3qSOlK2UU@cluster0-0hanq.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
